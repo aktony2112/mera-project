b1913dfeda718095a0dc3d12f3aac37f5a87a864
aktony2112 <107251680+aktony2112@users.noreply.github.com>
2022-08-16 15:33:46 +0530

    Add files via upload
    code mat liyo

---
+++ 2nd web/game webpage/home.js
@@ -0,0 +1,29 @@
+
+    
+//    functions to change pages in the website using js
+     
+   function login()
+    {
+        location="login.html"
+    }
+    function act()
+    {
+        location="Action.html"
+    }
+    function spo()
+    {
+        location="sports.html"
+    }
+    function rac()
+    {
+        location="race.html"
+    }
+    function sim()
+    {
+        location="simulation.html"
+    }
+
+        function back()
+    {
+        location="home.html"
+    }
